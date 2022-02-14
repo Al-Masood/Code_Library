86d6d66b78f10babf0d4dbca7f5852705a91e958
al-masood <98589987+al-masood@users.noreply.github.com>
2022-02-14 10:25:47 +0600

    Add files via upload

---
+++ lca.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll              long long
+#define l1(i, n)        for (ll i = 1; i <= n; i++)
+#define l0(i, n)        for (ll i = 0; i < n; i++)
+#define lo(i, s, n)     for (ll i = s; i <= n; i++)
+#define sca(n)          scanf("%lld", &n)
+#define pri(n)          printf("%lld", n)
+#define pb              push_back
+#define mp              make_pair
+#define sorted(x)       sort(x.begin(), x.end())
+#define reversed(x)     reverse(x.begin(), x.end())
+#define all(x)          x.begin(), x.end()
+#define ms(a, b)        memset(a, b, sizeof(a));
+#define cases(tc)       cout<<"Case "<<tc<<": "
+#define nl              cout<<"\n";
+#define pnl             printf("\n");
+#define pi              acos(-1)
+#define mod             1000000007
+#define inf             999999999999999999
+#define maxn            100001
+
+
+vector <ll> adj[maxn];
+ll par[maxn][19];
+ll lev[maxn];
+
+void dfs(ll a, ll p){
+    par[a][0]=p;
+    lev[a]=lev[p]+1;
+    for(ll i=1; i<=18; i++){
+        par[a][i]=par[par[a][i-1]][i-1];
+    }
+    for(ll i=0; i<adj[a].size(); i++){
+        if(adj[a][i]!=p) dfs(adj[a][i], a);
+    }
+    return;
+}
+ 
+ll lca(ll u, ll v) {
+    if (lev[u]<lev[v]) swap(u, v);
+    for(ll k=18; k>=0; k--) if (lev[par[u][k]] >= lev[v]) u = par[u][k];
+    if (u == v) return u;
+    for(ll k=18; k>=0; k--) if (par[u][k] != par[v][k]) u = par[u][k], v = par[v][k];
+    return par[u][0];
+}
+
+ 
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+ 
+
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+    }
+    return 0;
+}
---
+++ persistentsegtree.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll              long long
+#define l1(i, n)        for (ll i = 1; i <= n; i++)
+#define l0(i, n)        for (ll i = 0; i < n; i++)
+#define lo(i, s, n)     for (ll i = s; i <= n; i++)
+#define sca(n)          scanf("%lld", &n)
+#define pri(n)          printf("%lld", n)
+#define pb              push_back
+#define mp              make_pair
+#define sorted(x)       sort(x.begin(), x.end())
+#define reversed(x)     reverse(x.begin(), x.end())
+#define all(x)          x.begin(), x.end()
+#define ms(a, b)        memset(a, b, sizeof(a));
+#define cases(tc)       cout<<"Case "<<tc<<": "
+#define nl              cout<<"\n";
+#define pnl             printf("\n");
+#define pi              acos(-1)
+#define mod             1000000007
+#define inf             999999999999999999
+#define maxn            100001
+
+ll dt[maxn];
+ll seg[60*maxn];
+ll lef[60*maxn];
+ll rig[60*maxn];
+ll ver[maxn];
+ll nf=1;
+
+void build(ll st, ll en, ll nd){
+    if(st==en){
+        seg[nd]=dt[st];
+        return;
+    }
+    lef[nd]=++nf;
+    rig[nd]=++nf;
+    ll mid=(st+en)/2;
+    build(st, mid, lef[nd]);
+    build(mid+1, en, rig[nd]);
+    seg[nd]=seg[lef[nd]]+seg[rig[nd]];
+}
+
+ll update(ll st, ll en, ll nd, ll l, ll r, ll val){
+    if(st>r || en<l){
+        return nd;
+    }
+    ll nnd=++nf;
+    if(st>=l && en<=r){
+        seg[nnd]=seg[nd];
+        seg[nnd]+=val;
+        return nnd;
+    }
+    ll mid=(st+en)/2;
+    lef[nnd]=update(st, mid, lef[nd], l, r, val);
+    rig[nnd]=update(mid+1, en, rig[nd], l, r, val);
+    seg[nnd]=seg[lef[nnd]]+seg[rig[nnd]];
+    return nnd;
+}
+
+ll query(ll st, ll en, ll nd, ll l, ll r){
+    if(st>r || en<l){
+        return 0;
+    }
+    if(st>=l && en<=r){
+        return seg[nd];
+    }
+    ll mid=(st+en)/2;
+    return query(st, mid, lef[nd], l, r) + query(mid+1, en, rig[nd], l, r);
+}
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+ 
+
+    ll t;
+    t=1;
+    while(t--){
+        ll n;
+        cin>>n;
+    }
+    return 0;
+}

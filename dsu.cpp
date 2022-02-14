2bb4107219f0839186f921e4ffe2cf8e3d818327
al-masood <98589987+al-masood@users.noreply.github.com>
2022-02-14 10:50:39 +0600

    Add files via upload

---
+++ dsu.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll              long long
+#define l1(i, n)        for (ll i = 1; i <= n; i++)
+#define l0(i, n)        for (ll i = 0; i < n; i++)
+#define lo(i, s, n)     for (ll i = s; i <= n; i++)
+#define sca(n)          scanf("%lld", &n)
+#define pri(n)          printf("%lld", n)
+#define pb              push_back
+#define mp              make_pair
+#define sorted(x)       sort(x.begin(), x.end())
+#define reversed(x)     reverse(x.begin(), x.end())
+#define all(x)          x.begin(), x.end()
+#define ms(a, b)        memset(a, b, sizeof(a));
+#define cases(tc)       cout<<"Case "<<tc<<": "
+#define nl              cout<<"\n";
+#define pnl             printf("\n");
+#define pi              acos(-1)
+#define mod             1000000007
+#define inf             999999999999999999
+#define maxn            100001
+			
+
+
+ll cnt;
+ll par[10001];
+ll rnk[10001];
+ll sz[10001];
+
+void make_set(ll a){
+    par[a]=a;
+    rnk[a]=1;
+    sz[a]=1;
+}
+
+ll find(ll a){
+    if(a==par[a]) return a;
+    return par[a]=find(par[a]);
+}
+ 
+void merge(ll a, ll b){
+    ll p1=find(a);
+    ll p2=find(b);
+    if(p1==p2){
+        return;
+    }
+    if(p1>p2) swap(p1, p2);
+    par[p1]=p2;
+    sz[p2]+=sz[p1];
+    if(rnk[p1]==rnk[p2]) rnk[p2]++;
+    cnt--;
+}
+
+bool same(ll a, ll b){
+    return(par[a]==par[b]);
+}
+
+ll count(){
+    return cnt;
+}
+
+ll get_size(ll a){
+    return sz[par[a]];
+}
+ 
+ 
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+ 
+
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+    }
+    return 0;
+}
---
+++ sieve.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll              long long
+#define l1(i, n)        for (ll i = 1; i <= n; i++)
+#define l0(i, n)        for (ll i = 0; i < n; i++)
+#define lo(i, s, n)     for (ll i = s; i <= n; i++)
+#define sca(n)          scanf("%lld", &n)
+#define pri(n)          printf("%lld", n)
+#define pb              push_back
+#define mp              make_pair
+#define sorted(x)       sort(x.begin(), x.end())
+#define reversed(x)     reverse(x.begin(), x.end())
+#define all(x)          x.begin(), x.end()
+#define ms(a, b)        memset(a, b, sizeof(a));
+#define cases(tc)       cout<<"Case "<<tc<<": "
+#define nl              cout<<"\n";
+#define pnl             printf("\n");
+#define pi              acos(-1)
+#define mod             1000000007
+#define inf             999999999999999999
+#define maxn            100001
+			
+ 
+vector <ll> primes;
+bool chk[100001];
+
+void sieve(){
+    for(ll i=2; i*i<=100000; i++){
+        if(!chk[i]){
+            for(ll j=i*i; j<=100000; j+=i) chk[j]=true;
+        }
+    }
+
+    for(ll i=2; i<=100000; i++){
+        if(!chk[i]) primes.pb(i);
+    }
+}
+
+ 
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+ 
+
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+    }
+    return 0;
+}

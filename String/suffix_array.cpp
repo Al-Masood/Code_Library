f209d68b18c28bcf9e97bff0387d3f092d9964b3
Abdullah Al Masood <masood@Abdullahs-MacBook-Air.local>
2024-05-09 22:01:11 +0600

    Added instructions to Suffix Array

--- String/suffix_array.cpp
+++ String/suffix_array.cpp
@@ -1,3 +1,9 @@
+/*
+s=banana, s=banana$, m=7
+sa={$, a$, ana$, anana$, banana$, na$, nana$} = {6, 5, 3, 1, 0, 4, 2}
+lcp={0, 0, 1, 3, 0, 0, 2}
+*/
+
 #include <bits/stdc++.h>
  
 using namespace std;
@@ -17,57 +23,57 @@ using namespace std;
 #define pi              acos(-1)
 #define mod             1000000007
 #define inf             1000000000000000001
-#define maxn            200001
+#define maxn            200005
 
 
 
 
 string s;
-ll n, ra[maxn], tempra[maxn], sa[maxn], tempsa[maxn], lcp[maxn], plcp[maxn], phi[maxn], cnt[maxn];
+ll m, ra[maxn], tempra[maxn], sa[maxn], tempsa[maxn], lcp[maxn], plcp[maxn], phi[maxn], cnt[maxn];
 
 void countingsort(ll k){
     memset(cnt, 0, sizeof cnt);
-    for(ll i=0; i<n; i++){
-        if(i+k<n) cnt[ra[i+k]]++;
+    for(ll i=0; i<m; i++){
+        if(i+k<m) cnt[ra[i+k]]++;
         else cnt[0]++;
     }
-    ll mx=max(n, 300ll);
+    ll mx=max(m, 300ll);
     ll sum=0;
     for(ll i=0; i<mx; i++){
         ll temp=cnt[i];
         cnt[i]=sum;
         sum+=temp;
     }
-    for(ll i=0; i<n; i++){
-        if(sa[i]+k<n) tempsa[cnt[ra[sa[i]+k]]++]=sa[i];
+    for(ll i=0; i<m; i++){
+        if(sa[i]+k<m) tempsa[cnt[ra[sa[i]+k]]++]=sa[i];
         else tempsa[cnt[0]++]=sa[i];
     }
-    for(ll i=0; i<n; i++) sa[i]=tempsa[i];
+    for(ll i=0; i<m; i++) sa[i]=tempsa[i];
 }
 
  
 void suffixarray(){
-    for(ll i=0; i<n; i++) ra[i]=s[i];
-    for(ll i=0; i<n; i++) sa[i]=i;
-    for(ll k=1; k<n; k<<=1){
+    for(ll i=0; i<m; i++) ra[i]=s[i];
+    for(ll i=0; i<m; i++) sa[i]=i;
+    for(ll k=1; k<m; k<<=1){
         countingsort(k);
         countingsort(0);
         ll r=0;
         tempra[sa[0]]=r;
-        for(ll i=1; i<n; i++){
+        for(ll i=1; i<m; i++){
             if(ra[sa[i]]==ra[sa[i-1]] && ra[sa[i]+k]==ra[sa[i-1]+k]) tempra[sa[i]]=r;
             else tempra[sa[i]]=++r;
         }
-        for(ll i=0; i<n; i++) ra[i]=tempra[i];
-        if(ra[sa[n-1]]==n-1) break;
+        for(ll i=0; i<m; i++) ra[i]=tempra[i];
+        if(ra[sa[m-1]]==m-1) break;
     }
 
 }
 
 void lcprefix(){
     phi[sa[0]]=-1;
-    for(ll i=1; i<n; i++) phi[sa[i]]=sa[i-1];
-    for(ll i=0, len=0; i<n; i++){
+    for(ll i=1; i<m; i++) phi[sa[i]]=sa[i-1];
+    for(ll i=0, len=0; i<m; i++){
         if(phi[i]==-1){
             plcp[i]=0;
             continue;
@@ -77,7 +83,7 @@ void lcprefix(){
         len=max(len-1, 0ll);
     }
 
-    for(ll i=0; i<n; i++) lcp[i]=plcp[sa[i]];
+    for(ll i=0; i<m; i++) lcp[i]=plcp[sa[i]];
 }
  
 int main()
@@ -91,7 +97,7 @@ int main()
     while(t--){
         cin>>s;
         s+='$';
-        n=s.size();
+        m=s.size();
         suffixarray();
         lcprefix();
     }
